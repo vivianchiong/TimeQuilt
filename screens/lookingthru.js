a46231d452dde01508de2e1aac2f415a090c7348
Annie <anw104@ucsd.edu>
2021-02-19 16:49:03 -0800

    Skeleton Pages Created

---
+++ screens/lookingthru.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function LookingThrough() {
+  return (
+    <View >
+      <Text>Looking Through</Text>
+    </View>
+  );
+}
