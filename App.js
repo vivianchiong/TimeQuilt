d2133abfca823db7141fac7c244dabb04f980a6b
Annie <anw104@ucsd.edu>
2021-02-26 12:30:29 -0800

    Initial Post page created

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { StyleSheet, Text, View } from 'react-native';
+import CreatePost from './screens/createpost';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
-  );
+  const [fontsLoaded] = useFonts ({
+    'Rosarivo': require('./assets/fonts/Rosarivo-Regular.ttf'),
+    'Rosario': require('./assets/fonts/Rosario-VariableFont_wght.ttf')
+  })
+  if (fontsLoaded) {
+    return (
+      <CreatePost />
+    );
+  }
+  else {
+    return ( 
+      <AppLoading />
+    )
+  } 
 }
 
 const styles = StyleSheet.create({
