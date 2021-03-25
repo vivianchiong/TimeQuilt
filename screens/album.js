bb405e1278a4f7d72b875459d7d35dfd0320efa4
tht036 <77333776+tht036@users.noreply.github.com>
2021-03-25 15:50:41 -0700

    change the layout of album

--- screens/album.js
+++ screens/album.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Dimensions, StyleSheet, Image, Text, View, FlatList} from 'react-native';
 
 const{width}= Dimensions.get("window");
-const numColumns = 3;
 
 function Item({ item }) {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.titleWeek}>{item.week}</Text>
+      </View>
       <Image source={{uri:item.image}}  style={styles.image} />
     </View>
   );
@@ -16,32 +18,16 @@ export default class Album extends React.Component {
   state = {
     data:[
       {
-        day: 'Monday',
+        week: '03/01/2021 - 03/07/2021',
         image: 'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
       },
       {
-        day: 'Tuesday',
+        week: '03/08/2021 - 03/14/2021',
         image: 'https://images.unsplash.com/photo-1582102954017-c0ad7716d644?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
       },
       {
-        day: 'Wednesday',
+        week: '03/15/2021 - 03/21/2021',
         image: 'https://images.unsplash.com/photo-1542767673-ee5103fedbb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
-      },
-      {
-        day: 'Thursday',
-        image: 'https://images.unsplash.com/photo-1528650765831-7f2254800a83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
-      },
-      {
-        day: 'Friday',
-        image: 'https://images.unsplash.com/photo-1516836378273-db6cea41d84c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
-      },
-      {
-        day: 'Saturday',
-        image: 'https://images.unsplash.com/photo-1422544834386-d121ef7c6ea8?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
-      },
-      {
-        day: 'Sunday',
-        image: 'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
       },
     ]
   }
@@ -50,12 +36,10 @@ export default class Album extends React.Component {
     return (
       <View style = {styles.container}>
         <Text style={styles.titlePage}>Album</Text>
-        <Text style={styles.titleWeek}> 03/01/2021 - 03/07/2021</Text>
         <FlatList
           data={this.state.data}
           renderItem={({ item }) => <Item item={item}/>}
-          keyExtractor={item => item.day}
-          numColumns={numColumns}
+          keyExtractor={item => item.week}
         />
       </View>
     );
@@ -87,8 +71,8 @@ const styles = StyleSheet.create({
     fontFamily: 'Rosarivo',
   },
   image: {
-    width: width/numColumns, 
-    height: width/numColumns,
+    width: width*0.8, 
+    height: width*0.8,
     alignSelf: 'center',
   },
 });
