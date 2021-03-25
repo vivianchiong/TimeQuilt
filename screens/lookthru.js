d83b5a2806fa8211ea4ac7f01774b5a72ccf8d3c
tht036 <77333776+tht036@users.noreply.github.com>
2021-03-25 12:22:25 -0700

    finish lookthrough page

--- screens/lookthru.js
+++ screens/lookthru.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Image, Text, View, FlatList, TouchableWithoutFeedback} from 'react-native';
+import {Dimensions, StyleSheet, Image, Text, View, FlatList} from 'react-native';
 
 const{width}= Dimensions.get("window");
 
@@ -44,25 +44,25 @@ export default class LookThru extends React.Component {
         day: 'Thursday',
         date: '03/04/2021',
         description: 'ayyy!',
-        image: 'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+        image: 'https://images.unsplash.com/photo-1528650765831-7f2254800a83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
       },
       {
         day: 'Friday',
         date: '03/05/2021',
         description: 'ayyy!',
-        image: 'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+        image: 'https://images.unsplash.com/photo-1516836378273-db6cea41d84c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
       },
       {
         day: 'Saturday',
         date: '03/06/2021',
         description: 'ayyy!',
-        image: 'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
+        image: 'https://images.unsplash.com/photo-1422544834386-d121ef7c6ea8?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
       },
       {
         day: 'Sunday',
         date: '03/07/2021',
         description: 'ayyy!',
-        image: 'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+        image: 'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
       }
     ]
   }
@@ -75,7 +75,7 @@ export default class LookThru extends React.Component {
           renderItem={({ item }) => <PostItem item={item}/>}
           keyExtractor={item => item.date}
           horizontal
-          showsHorizontalScrollIndicator
+          showsHorizontalScrollIndicator={false}
           pagingEnabled
           bounces={false}
         />
@@ -85,6 +85,7 @@ export default class LookThru extends React.Component {
 }
 const styles = StyleSheet.create({
   container:{
+    width: width,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -108,10 +109,10 @@ const styles = StyleSheet.create({
   image: {
     width: width*0.8,
     height: width*0.8,
-    resizeMode: 'center'
+    justifyContent: 'center'
   },
   description: {
-    marginBottom: 75,
+    marginTop: 30,
     color: '#ffffff',
     fontStyle: 'normal',
     fontWeight: 'normal',
@@ -119,68 +120,3 @@ const styles = StyleSheet.create({
     fontFamily: 'Rosarivo',
   }
 });
-
-// import {View, Image, ScrollView, Dimensions, Text, StyleSheet} from 'react-native';
-
-// const{width}=Dimensions.get("window");
-// const height = width * 0.8;
-// const images=[
-//   'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1582102954017-c0ad7716d644?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1542767673-ee5103fedbb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1528650765831-7f2254800a83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1516836378273-db6cea41d84c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1422544834386-d121ef7c6ea8?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
-//   'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
-// ]
-
-// export default class LookThru extends React.Component{
-//   state={
-//     active: 0
-//   }
-//   change=({nativeEvent}) =>{
-//     const slide=Math.ceil(nativeEvent.contentOffset.x/nativeEvent.layoutMeasurement.width);
-//     if(slide!==this.state.active){
-//       this.setState({active: slide});
-//     }
-//   }
-//   render(){
-//     return(
-//       <View style={style.imageview}>
-//         <ScrollView
-//           pagingEnabled
-//           horizontal
-//           showsHorizontalScrollIndicator={false}
-//           onScroll={this.change}
-//           style={style.scroll}
-//         >
-//           {
-//             images.map((image, index) => (
-//               <Image
-//                 key={index}
-//                 source={{uri: image}}
-//                 style={style.image}/>
-//             ))
-//           }
-//         </ScrollView>
-//         <View style={style.indicatorview}>
-//           {
-//             images.map((i, k) =>(
-//               <Text key={k} style={k==this.state.active ? style.indicatoractive: style.indicator}>⬤</Text>
-//             ))
-//           }
-
-//         </View>
-//       </View>
-//     )
-//   }
-// }
-
-// const style = StyleSheet.create({
-//   imageview: {marginTop: 200, width, height},
-//   scroll: {width, height},
-//   image: {width, height, resizeMode: 'center'},
-//   indicatorview: {flexDirection:'row', position:'absolute', bottom:0, alignSelf:'center'},
-//   indicator: {fontSize:(width/30), color:'#888', margin: 5},
-//   indicatoractive: {fontSize:(width/30), color:'#fff', margin: 5}
-// })
