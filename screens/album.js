71540f8aa611ceb6ba5c1bc382ce385f66a67b9f
tht036 <77333776+tht036@users.noreply.github.com>
2021-03-29 16:22:01 -0700

    experiment with sectionlist

--- screens/album.js
+++ screens/album.js
@@ -1,45 +1,65 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Image, Text, View, FlatList} from 'react-native';
+import {Dimensions, StyleSheet, Image, Text, View, SectionList} from 'react-native';
 
 const{width}= Dimensions.get("window");
 
 function Item({ item }) {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.titleWeek}>{item.week}</Text>
-      </View>
-      <Image source={{uri:item.image}}  style={styles.image} />
+    <View style={{flexDirection: 'row',
+    flexWrap: 'wrap'}}>
+      <Image source={{uri:item}}  style={styles.image} />
     </View>
   );
 }
 
 export default class Album extends React.Component {
-  state = {
-    data:[
-      {
-        week: '03/01/2021 - 03/07/2021',
-        image: 'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
-      },
-      {
-        week: '03/08/2021 - 03/14/2021',
-        image: 'https://images.unsplash.com/photo-1582102954017-c0ad7716d644?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
-      },
-      {
-        week: '03/15/2021 - 03/21/2021',
-        image: 'https://images.unsplash.com/photo-1542767673-ee5103fedbb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60'
-      },
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:[
+        {
+          week: '03/01/2021 - 03/07/2021',
+          data: [
+            'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1582102954017-c0ad7716d644?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1542767673-ee5103fedbb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1528650765831-7f2254800a83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1516836378273-db6cea41d84c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1422544834386-d121ef7c6ea8?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
+          ]
+
+        },
+        {
+          week: '03/08/2021 - 03/14/2021',
+          data: [
+            'https://images.unsplash.com/photo-1562285061-26601e60f0f3?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1582102954017-c0ad7716d644?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1542767673-ee5103fedbb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1528650765831-7f2254800a83?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1516836378273-db6cea41d84c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1422544834386-d121ef7c6ea8?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3wxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60',
+            'https://images.unsplash.com/photo-1578490057216-f69104fbf402?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8OXwxMTQ4ODQ1Nnx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=60'
+          ]
+        },
+      ]
+    }
   }
 
   render(){
     return (
       <View style = {styles.container}>
         <Text style={styles.titlePage}>Album</Text>
-        <FlatList
-          data={this.state.data}
+        <SectionList
+          // contentContainerStyle={{flexDirection: 'row',
+          // flexWrap: 'wrap'}}
+          sections={this.state.data}
+          keyExtractor={(item, index) => item + index}
           renderItem={({ item }) => <Item item={item}/>}
-          keyExtractor={item => item.week}
+          renderSectionHeader={({ section: { week } }) => 
+            <Text style={styles.titleWeek}>{week}</Text>
+          }
+          initialNumToRender={2}
         />
       </View>
     );
@@ -71,8 +91,8 @@ const styles = StyleSheet.create({
     fontFamily: 'Rosarivo',
   },
   image: {
-    width: width*0.8, 
-    height: width*0.8,
+    width: width*0.4, 
+    height: width*0.4,
     alignSelf: 'center',
   },
 });
